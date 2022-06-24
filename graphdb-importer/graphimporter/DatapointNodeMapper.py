d07a8d3bbdd1c364bd9a00b0dda9e516b87ab868
ahahn94 <ahahn94@outlook.com>
2022-06-24 13:13:14 +0200

    Add Datapoint-Node-Mapper
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

---
+++ graphdb-importer/graphimporter/DatapointNodeMapper.py
@@ -0,0 +1,28 @@
+from graphimporter.entities.Datapoint import Datapoint
+
+
+class DatapointNodeMapper:
+
+    def entity_to_node_string(self, entity: Datapoint):
+        template = "Datapoint {{{coordinates}, " \
+                   "{values}}}"
+        return template.format(coordinates=self.__get_prepared_coordinates_string(entity),
+                               values=self.__get_prepared_values_string(entity))
+
+    def entity_to_node_coordinates_string(self, entity: Datapoint):
+        template = "Datapoint {{{coordinates}}}"
+        return template.format(coordinates=self.__get_prepared_coordinates_string(entity))
+
+    def node_to_entity(self, node):
+        return Datapoint(node["stateName"], node["countyName"], node["ageGroup"], node["gender"], node["date"],
+                         node["casesCount"], node["deathsCount"], node["recoveredCount"])
+
+    def __get_prepared_coordinates_string(self, entity: Datapoint):
+        template = "stateName: \"{state}\", countyName: \"{county}\", ageGroup: \"{age_group}\", " \
+                   "gender: \"{gender}\", date: \"{date}\""
+        return template.format(state=entity.get_state(), county=entity.get_county(), age_group=entity.get_age_group(),
+                               gender=entity.get_gender(), date=entity.get_date())
+
+    def __get_prepared_values_string(self, entity: Datapoint):
+        template = "casesCount: {cases}, deathsCount: {deaths}, recoveredCount: {recovered}"
+        return template.format(cases=entity.get_cases(), deaths=entity.get_deaths(), recovered=entity.get_recovered())
---
+++ graphdb-importer/graphimporter/tests/test_DatapointNodeMapper.py
@@ -0,0 +1,42 @@
+import unittest
+
+from graphimporter.DatapointNodeMapper import DatapointNodeMapper
+from graphimporter.entities.Datapoint import Datapoint
+
+
+class DatapointNodeMapperTest(unittest.TestCase):
+    __test_datapoint = Datapoint("Test-Land", "Test-Kreis A", "00-04", "X", "2022-06-21", 0, 0, 0)
+    __test_node = {
+        "stateName": "Test-Land",
+        "countyName": "Test-Kreis A",
+        "ageGroup": "00-04",
+        "gender": "X",
+        "date": "2022-06-21",
+        "casesCount": 0,
+        "deathsCount": 0,
+        "recoveredCount": 0
+    }
+
+    @classmethod
+    def setUpClass(cls):
+        cls.__datapoint_node_mapper = DatapointNodeMapper()
+
+    def test_datapoint_to_node_string(self):
+        expected = 'Datapoint {stateName: "Test-Land", countyName: "Test-Kreis A", ageGroup: "00-04", gender: "X", ' \
+                   'date: "2022-06-21", casesCount: 0, deathsCount: 0, recoveredCount: 0}'
+        node_string = self.__datapoint_node_mapper.entity_to_node_string(self.__test_datapoint)
+        self.assertEqual(node_string, expected)
+
+    def test_datapoint_to_node_coordinates_string(self):
+        expected = 'Datapoint {stateName: "Test-Land", countyName: "Test-Kreis A", ageGroup: "00-04", gender: "X", ' \
+                   'date: "2022-06-21"}'
+        node_string = self.__datapoint_node_mapper.entity_to_node_coordinates_string(self.__test_datapoint)
+        self.assertEqual(node_string, expected)
+
+    def test_node_to_entity(self):
+        entity = self.__datapoint_node_mapper.node_to_entity(self.__test_node)
+        self.assertEqual(entity, self.__test_datapoint)
+
+
+if __name__ == '__main__':
+    unittest.main()
