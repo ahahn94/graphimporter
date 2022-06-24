d784a6c543d08c185d55b8bd2e754373a1d8d297
ahahn94 <ahahn94@outlook.com>
2022-06-24 14:32:36 +0200

    Clean-Up Of Neo4jRepository
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- graphdb-importer/graphimporter/repositories/Neo4jRepository.py
+++ graphdb-importer/graphimporter/repositories/Neo4jRepository.py
@@ -1,14 +1,15 @@
-import neo4j.graph
-
+from graphimporter.DatapointNodeMapper import DatapointNodeMapper
 from graphimporter.Neo4jDatabaseConnection import Neo4jDatabaseConnection
 from graphimporter.entities.Datapoint import Datapoint
 
 
 class Neo4jRepository:
     __neo4j_database_connection: Neo4jDatabaseConnection
+    __datapoint_node_mapper: DatapointNodeMapper
 
-    def __init__(self, neo4j_database_connection):
+    def __init__(self, neo4j_database_connection, datapoint_node_mapper: DatapointNodeMapper):
         self.__neo4j_database_connection = neo4j_database_connection
+        self.__datapoint_node_mapper = datapoint_node_mapper
 
     def initialize(self):
         if not self.__neo4j_database_connection.is_initialized():
@@ -17,40 +18,33 @@ class Neo4jRepository:
     def is_initialized(self):
         return self.__neo4j_database_connection.is_initialized()
 
-    def insert_datapoint(self, datapoint):
+    def insert_datapoint(self, datapoint: Datapoint):
         statement = self.__get_create_statement_for_datapoint(datapoint)
         self.__neo4j_database_connection.run_query(statement)
 
-    def delete_datapoint(self, datapoint):
+    def delete_datapoint(self, datapoint: Datapoint):
         statement = self.__get_delete_statement_for_datapoint(datapoint)
         self.__neo4j_database_connection.run_query(statement)
 
-    def read_datapoint(self, date, county, age_group, gender):
-        statement = f'MATCH (r: Record {{date: "{date}", countyName: "{county}", ageGroup: "{age_group}", ' \
-                    f'gender: "{gender}"}}) RETURN r LIMIT 1'
+    def read_datapoint(self, datapoint: Datapoint):
+        template = 'MATCH (d: {datapointCoordinates}) RETURN d LIMIT 1'
+        datapoint_coordinates = self.__datapoint_node_mapper.entity_to_node_coordinates_string(datapoint)
+        statement = template.format(datapointCoordinates=datapoint_coordinates)
         result = self.__neo4j_database_connection.run_query(statement)
-        return self.__node_to_dataset(result[0][0])
+        return self.__datapoint_node_mapper.node_to_entity(result[0][0])
 
-    @staticmethod
-    def __get_create_statement_for_datapoint(datapoint):
-        return f"CREATE (r:Record {{" \
-               f"stateName: \"{datapoint.get_state()}\", " \
-               f"countyName: \"{datapoint.get_county()}\", " \
-               f"ageGroup: \"{datapoint.get_age_group()}\", " \
-               f"gender: \"{datapoint.get_gender()}\", " \
-               f"date: \"{datapoint.get_date()}\", " \
-               f"casesCount: {datapoint.get_cases()}, " \
-               f"deathsCount: {datapoint.get_deaths()}, " \
-               f"recoveredCount: {datapoint.get_recovered()}" \
-               f"}});"
+    def datapoint_exists(self, datapoint: Datapoint) -> bool:
+        template = "OPTIONAL MATCH (d: {datapointDefinition}) RETURN d IS NOT NULL AS Exists"
+        statement = template.format(datapointDefinition=self.__datapoint_node_mapper.entity_to_node_string(datapoint))
+        result_2d_array = self.__neo4j_database_connection.run_query(statement)
+        exists = result_2d_array[0][0]
+        return exists
 
-    @staticmethod
-    def __get_delete_statement_for_datapoint(datapoint):
-        return f"MATCH (r:Record {{stateName: \"{datapoint.get_state()}\", countyName: \"{datapoint.get_county()}\", " \
-               f"ageGroup: \"{datapoint.get_age_group()}\", gender: \"{datapoint.get_gender()}\", " \
-               f"date: \"{datapoint.get_date()}\"}}) DELETE r;"
+    def __get_create_statement_for_datapoint(self, datapoint: Datapoint):
+        template = "CREATE (d: {datapointDefinition});"
+        return template.format(datapointDefinition=self.__datapoint_node_mapper.entity_to_node_string(datapoint))
 
-    @staticmethod
-    def __node_to_dataset(node: neo4j.graph.Node):
-        return Datapoint(node["stateName"], node["countyName"], node["ageGroup"], node["gender"], node["date"],
-                         node["casesCount"], node["deathsCount"], node["recoveredCount"])
+    def __get_delete_statement_for_datapoint(self, datapoint: Datapoint):
+        template = "MATCH (d: {datapointCoordinates}) DELETE d;"
+        datapoint_coordinates = self.__datapoint_node_mapper.entity_to_node_coordinates_string(datapoint)
+        return template.format(datapointCoordinates=datapoint_coordinates)
--- graphdb-importer/graphimporter/tests/test_Neo4jRepository.py
+++ graphdb-importer/graphimporter/tests/test_Neo4jRepository.py
@@ -1,5 +1,6 @@
 import unittest
 
+from graphimporter.DatapointNodeMapper import DatapointNodeMapper
 from graphimporter.Neo4jDatabaseConnection import Neo4jDatabaseConnection
 from graphimporter.entities.Datapoint import Datapoint
 from graphimporter.repositories.Neo4jRepository import Neo4jRepository
@@ -10,32 +11,47 @@ class Neo4jRepositoryTest(unittest.TestCase):
     __username: str = "neo4j"
     __password: str = "graphdb"
     __neo4j_repository: Neo4jRepository = None
+    __datapoint_node_mapper: DatapointNodeMapper = None
 
-    __test_datapoint_1 = Datapoint("Test-Land", "Test-Kreis A", "00-04", "X", "2022-06-21", 0, 0, 0)
+    __datapoint_insert_delete = Datapoint("Test-Land", "Test-Kreis A", "00-04", "X", "2022-06-21", 0, 0, 0)
+    __datapoint_setup_exists_read_teardown = Datapoint("Test-Land", "Test-Kreis B", "00-04", "X", "2022-06-21", 0, 0, 0)
 
     @classmethod
     def setUpClass(cls):
-        cls.__neo4j_repository = Neo4jRepository(
-            Neo4jDatabaseConnection(cls.__server_uri, cls.__username, cls.__password))
+        neo4j_database_connection = Neo4jDatabaseConnection(cls.__server_uri, cls.__username, cls.__password)
+        cls.__datapoint_node_mapper = DatapointNodeMapper()
+        cls.__neo4j_repository = Neo4jRepository(neo4j_database_connection, cls.__datapoint_node_mapper)
         cls.__neo4j_repository.initialize()
+        cls.__neo4j_repository.insert_datapoint(cls.__datapoint_setup_exists_read_teardown)
 
     def test_initialize(self):
-        neo4j_repository = Neo4jRepository(Neo4jDatabaseConnection(self.__server_uri, self.__username, self.__password))
+        neo4j_database_connection = Neo4jDatabaseConnection(self.__server_uri, self.__username, self.__password)
+        neo4j_repository = Neo4jRepository(neo4j_database_connection, self.__datapoint_node_mapper)
         neo4j_repository.initialize()
         self.assertEqual(neo4j_repository.is_initialized(), True)
 
-    def test_insert_and_read_datapoint(self):
-        self.__neo4j_repository.insert_datapoint(self.__test_datapoint_1)
-        datapoint = self.__neo4j_repository.read_datapoint(
-            self.__test_datapoint_1.get_date(),
-            self.__test_datapoint_1.get_county(),
-            self.__test_datapoint_1.get_age_group(),
-            self.__test_datapoint_1.get_gender())
-        self.assertEqual(datapoint, self.__test_datapoint_1)
+    def test_insert_delete_datapoint(self):
+        self.__test_insert()
+        self.__test_delete()
+
+    def __test_insert(self):
+        self.__neo4j_repository.insert_datapoint(self.__datapoint_insert_delete)
+        self.assertEqual(self.__neo4j_repository.datapoint_exists(self.__datapoint_insert_delete), True)
+
+    def __test_delete(self):
+        self.__neo4j_repository.delete_datapoint(self.__datapoint_insert_delete)
+        self.assertEqual(self.__neo4j_repository.datapoint_exists(self.__datapoint_insert_delete), False)
+
+    def test_datapoint_exists(self):
+        self.assertEqual(self.__neo4j_repository.datapoint_exists(self.__datapoint_setup_exists_read_teardown), True)
+
+    def test_read_datapoint(self):
+        datapoint_from_database = self.__neo4j_repository.read_datapoint(self.__datapoint_setup_exists_read_teardown)
+        self.assertEqual(datapoint_from_database, self.__datapoint_setup_exists_read_teardown)
 
     @classmethod
     def tearDownClass(cls):
-        cls.__neo4j_repository.delete_datapoint(cls.__test_datapoint_1)
+        cls.__neo4j_repository.delete_datapoint(cls.__datapoint_setup_exists_read_teardown)
 
 
 if __name__ == '__main__':
